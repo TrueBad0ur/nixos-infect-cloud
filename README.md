43df8ed95cbd997a622244ba50995df2faa9bf56
TrueBad0ur <pieceofcakecupofcoffee@gmail.com>
2024-09-05 02:27:55 +0300

    Clear configs

--- README.md
+++ README.md
@@ -9,5 +9,5 @@ After that we can connect via root (also didn't process the right way)
 #ToDo
 
 - [ ] Write for yandex cloud
-- [ ] Write processing for right ansible reboot machine exception
+- [X] Write processing for right ansible reboot machine exception
 - [ ] Write processing for not only root access
--- run.sh
+++ run.sh
@@ -21,14 +21,21 @@ if [ $1 == "--yandex" ] || [ $1 == "-y" ]; then
   echo "Pseudo loading to wait when the infra is set up"
   (cd ansible && ansible-playbook ./playbooks/ab/main.yml)
 elif [ $1 == "--vk" ] || [ $1 == "-v" ]; then
-  ip=$(cd ./terraform/terraform-vk-cloud && terraform init && terraform fmt && terraform validate && terraform plan && terraform apply -auto-approve | tee /dev/tty)
-  global_ip=$(echo "$ip" | tail -1 | cut -d' ' -f3)
-  #zsh specific
-  global_ip="$global_ip" | cut -c2- | rev | cut -c2- | rev
-  #macOS specific
-  sed -i '' -E "s/((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])/$global_ip/" ./ansible/hosts
+  (cd ./terraform/terraform-vk-cloud && terraform init && terraform fmt && terraform validate && terraform plan && terraform apply -auto-approve)
+  ip=$(cd ./terraform/terraform-vk-cloud && terraform output -raw instance_fip)
 
+  echo -ne "[hosts]\nhost1 ansible_host=$ip ansible_user=debian\n\n[all:vars]\nansible_ssh_private_key_file=~/.ssh/id_rsa_public" > ansible/hosts
+  echo "Little sleep when infra is loading"
+  sleep 10
   (cd ansible && ansible-playbook -i hosts install.yml)
+
+  echo "Nix installed, starting nixos-infect installation"
+  ssh debian@"$ip" -i ~/.ssh/id_rsa_public "sudo ./nixos-infect"
+
+  echo "Little sleep when infra is rebooting"
+  sleep 10
+
+  echo "And now: ssh root@$ip -i ~/.ssh/id_rsa_public"
 elif [ $1 == "--help" ] || [ $1 == "-h" ]; then
   help
 elif [ $1 == "--destroyyandex" ] || [ $1 == "-dy" ]; then
